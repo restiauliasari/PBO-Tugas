dd2d0c5f1974a8fac53695a1576681e7b47f50e0
restiauliasari <114378007+restiauliasari@users.noreply.github.com>
2022-12-22 08:09:35 +0700

    Add files via upload

---
+++ model/Anggota.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package RestiAuliaSari.model;
+
+/**
+ *
+ * @author USER
+ */
+public class Anggota {
+    private String nobp;
+    private String nama;
+    private String alamat;
+    
+    public String getNobp() {
+        return nobp;
+    }
+    public void setNobp(String nobp) {
+        this.nobp = nobp;
+    }
+    
+    public String getNama() {
+        return nama;
+    }
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+    
+     public String getAlamat() {
+        return alamat;
+    }
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
+}
