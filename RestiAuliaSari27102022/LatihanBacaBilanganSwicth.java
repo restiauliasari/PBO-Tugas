43f7053d9720ba8c059799cec02a1fb27ec71e95
restiauliasari <114378007+restiauliasari@users.noreply.github.com>
2022-12-23 19:14:30 +0700

    Add files via upload

---
+++ RestiAuliaSari27102022/LatihanBacaBilanganSwicth.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package RestiAuliaSari27102022;
+import java.util.Scanner;
+/**
+ *
+ * @author acer
+ */
+public class LatihanBacaBilanganSwicth {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        int bil;
+        
+        System.out.print("Masukkan Angka : ");
+        bil = in.nextInt();
+        
+        switch(bil){
+            case 1 : System.out.println("Valid Number");
+            break;
+            
+             case 2 : System.out.println("Valid Number");
+            break;
+            
+             case 3 : System.out.println("Valid Number");
+            break;
+            
+             case 4 : System.out.println("Valid Number");
+            break;
+            
+             case 5 : System.out.println("Valid Number");
+            break;
+            
+             case 6 : System.out.println("Valid Number");
+            break;
+            
+             case 7 : System.out.println("Valid Number");
+            break;
+            
+             case 8 : System.out.println("Valid Number");
+            break;
+            
+             case 9 : System.out.println("Valid Number");
+            break;
+            
+             case 10 : System.out.println("Valid Number");
+            break;
+            
+             default :
+                 System.out.println("Invalid Number");
+            
+            
+        }
+    }
+}
