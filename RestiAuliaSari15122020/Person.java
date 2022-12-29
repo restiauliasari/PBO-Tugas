e2cd55fe8871926c8b7b82459af8617ee0b470f7
restiauliasari <114378007+restiauliasari@users.noreply.github.com>
2022-12-29 12:34:19 +0700

    Add files via upload

--- RestiAuliaSari15122020/Person.java
+++ RestiAuliaSari15122020/Person.java
@@ -15,8 +15,8 @@ public class Person {
     
     public Person(){ 
         System.out.println("Inside Person:Constructor"); 
-        name = ""; 
-        address = ""; 
+        name = "Anna"; 
+        address = "Padang"; 
     } 
     public Person( String name, String address ){ 
         this.name = name; 
@@ -25,14 +25,17 @@ public class Person {
     public String getName(){ 
         System.out.println("getName Person");
         return name; 
-    } 
-    public String getAddress(){ 
-        return address; 
-    } 
+    }  
     public void setName( String name ){ 
         this.name = name; 
+    } 
+     public String getAddress(){ 
+        return address; 
     } 
     public void setAddress( String add ){ 
         this.address = add; 
     } 
+    public static void main (String[] args){
+        Person p = new Person();
+    }
 }
--- RestiAuliaSari15122020/Student.java
+++ RestiAuliaSari15122020/Student.java
@@ -10,17 +10,32 @@ package RestiAuliaSari15122020;
  * @author USER
  */
 public class Student extends Person {
+    
+    private String nobp;
+    
     public Student(){ 
         super ("Anna","Padang");
         System.out.println("Inside Student:Constructor"); 
     } 
-    public static void main( String[] args ){ 
-        Student anna = new Student(); 
-        System.out.println(anna.name);
-        System.out.println(anna.address);
-    }
-     public String getName(){ 
-        System.out.println("getName Student");
+    
+    @Override
+    public String getName(){ 
+        System.out.println("get Name Student");
         return name; 
-    } 
+    }
+    
+    public String getNobp(){ 
+        System.out.println("get Name Student");
+        return nobp; 
+    }
+    
+    public static void main( String[] args ){ 
+        Student anna = new Student();
+//        anna.name ="Anna";
+//        anna.address ="Padang";
+//        anna.nobp ="1111111";
+        System.out.println(anna.name);
+//        System.out.println(anna.address);
+//        System.out.println(anna.nobp);
+    }
 }
